d1ca86ec451cc3d3b8b494e63a5eb0dec32d21b6
mziyambi <mziyam@mtc.ca.gov>
2016-05-13 11:07:23 -0700

    Scaffold commit

---
+++ client/app/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('ptapApp', [
+  'ptapApp.auth',
+  'ptapApp.admin',
+  'ptapApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap',
+  'validation.match'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
